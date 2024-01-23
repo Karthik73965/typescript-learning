c6961e8e6d7bb8f3d27c424f2cc7445b7b76838c
Karthik73965 <karthikjidagam81@gmail.com>
2024-01-23 09:48:44 +0530

    types in ts

---
+++ types.ts
@@ -0,0 +1,15 @@
+//string
+let karthik: string = "karthik"
+
+//number 
+let id :number = 1
+
+//boolean 
+let attendance :boolean = false 
+ 
+//any 
+let things :any = "S"
+
+
+//array 
+let items :number[]= [1, 2 ,2]
