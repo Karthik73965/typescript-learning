2534bfc1d0763ebd983b05dfbbe7da818e857da0
Karthik73965 <karthikjidagam81@gmail.com>
2024-01-23 11:32:06 +0530

    tuples in type script

---
+++ Tuples.ts
@@ -0,0 +1,12 @@
+//this was the normal array 
+const array :number[] = []
+
+// tuples - basically we are declaring before using it 
+const array1 :[string, number , boolean]= ["kartik", 2, true]
+
+// through type 
+type array3 = [string , number]
+
+const array2 :array3 = ["karthik", 1]
+
+// this was a use full thing in tyep script to strictly wright the things up 
